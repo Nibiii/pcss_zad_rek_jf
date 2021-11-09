8e5e588331d36ce94a798e554f6de3666c4962c7
JFedoruk <janfedoruk@gmail.com>
2021-11-09 23:27:54 +0100

    modified:   app.py

--- app.py
+++ app.py
@@ -23,43 +23,43 @@ def register_ip():
 
 # main site with visitor ip information
 @app.route('/')
-@accept('text/html')
+@accept('text/html') # for clients requesting html content
 def index():
     register_ip()
-    return render_template("main.html", visitor_ip = ip_address), 200, {'Content-Type': 'text/html; charset=utf-8'}
+    return render_template("main.html", visitor_ip = ip_address), 200, {'Content-Type': 'text/html; charset=utf-8'} # return html file containing user IP
 
-@index.support('text/plain')
+@index.support('text/plain') # for clients requesting txt content
 def index_txt():
     register_ip()
-    return render_template("main.txt", visitor_ip = ip_address), 200, {'Content-Type': 'text/plain; charset=utf-8'}
+    return render_template("main.txt", visitor_ip = ip_address), 200, {'Content-Type': 'text/plain; charset=utf-8'} # return txt file containing user IP
 
-@index.support('text/xml')
+@index.support('text/xml') # for clients requesting xml content
 def index_xml():
     register_ip()
-    return render_template("main.xml", visitor_ip = ip_address), 200, {'Content-Type': 'text/xml; charset=utf-8'}
+    return render_template("main.xml", visitor_ip = ip_address), 200, {'Content-Type': 'text/xml; charset=utf-8'} # return xml file containing user IP
 
-@index.support('text/yaml')
+@index.support('text/yaml') # for clients requesting yaml content
 def index_yaml():
     register_ip()
-    return render_template("main.yaml", visitor_ip = ip_address), 200, {'Content-Type': 'text/yaml; charset=utf-8'}
+    return render_template("main.yaml", visitor_ip = ip_address), 200, {'Content-Type': 'text/yaml; charset=utf-8'} # return yaml file containing user IP
 
 # secondary site with list containing all visitorss' ip
 @app.route('/history')
-@accept('text/html')
+@accept('text/html') # for clients requesting html content
 def history():
-    return render_template("history.html", data=ip_list), 200, {'Content-Type': 'text/html; charset=utf-8'}
+    return render_template("history.html", data=ip_list), 200, {'Content-Type': 'text/html; charset=utf-8'} # return html file containing all previous IPs and it's request time
 
-@history.support('text/plain')
+@history.support('text/plain') # for clients requesting txt content
 def history_txt():
-    return render_template("history.txt", data=ip_list), 200, {'Content-Type': 'text/plain; charset=utf-8'}
+    return render_template("history.txt", data=ip_list), 200, {'Content-Type': 'text/plain; charset=utf-8'} # return txt file containing all previous IPs and it's request time
 
-@history.support('text/xml')
+@history.support('text/xml') # for clients requesting xml content
 def history_xml():
-    return render_template("history.xml", data=ip_list), 200, {'Content-Type': 'text/xml; charset=utf-8'}
+    return render_template("history.xml", data=ip_list), 200, {'Content-Type': 'text/xml; charset=utf-8'} # return xml file containing all previous IPs and it's request time
 
-@history.support('text/yaml')
+@history.support('text/yaml') # for clients requesting yaml content
 def history_yaml():
-    return render_template("history.yaml", data=ip_list), 200, {'Content-Type': 'text/yaml; charset=utf-8'}
+    return render_template("history.yaml", data=ip_list), 200, {'Content-Type': 'text/yaml; charset=utf-8'} # return yaml file containing all previous IPs and it's request time
 
 # use filter to convert timestamp into readable date
 @app.template_filter('ctime')
